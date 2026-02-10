d69d5cf8e1752862022f20419cfc06d542546db4
IonHaruta <harutaion0@gmail.com>
2026-02-10 17:18:08 +0200

    Configure GitHub Pages build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/toy-subscription-box/",
   server: {
     host: "::",
     port: 8080,
